568026d5e60e2710ccf05c61d36ce1796e2e1b89
Lee Pavelich <leepavelich@gmail.com>
2022-01-19 05:36:20 +0000

    Can enter a breed as a command-line arg and handles errors

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,30 @@
 const request = require ('request');
 
-const breed = process.argv.slice(2);
+let breed = process.argv.slice(2);
 
-if (breed.length !== 1) {
-  console.log('Please enter only one breed')
+if (breed.length !== 1 && breed.length !== 2) {
+  console.log('Please enter only one breed.')
   return;
 }
 
-const search = `https://api.thecatapi.com/v1/breeds/search?q=${breeDd}`;
+if (breed.length === 2) {
+  breed = breed.join(' ');
+}
+
+const search = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
 request(search, (error, response, body) => {
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
+  if (error) {
+    console.log('Error', error)
+    return;
+  }
 
   const data = JSON.parse(body);
+
+  if (data[0] === undefined) {
+    console.log('Breed not found. Please enter a valid breed.');
+    return;
+  }
+
   console.log(data[0].description)
 });
